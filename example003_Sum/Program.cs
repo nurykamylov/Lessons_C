260318b26d3a73e3cd399bdbadb0d323d76c7755
Nurik <you@example.com>
2022-10-16 20:01:24 +0300

    Random intergers

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 7;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
