c9bab95923587c609900b9a84f4dac5e5ec16d4f
Nurik <you@example.com>
2022-10-16 23:12:53 +0300

    example005 Conditions

---
+++ example005_Conditions/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Type the username:");
+string username = Console.ReadLine();
+if (username.ToLower() == "bret")
+{
+    Console.WriteLine("Ura,this is Bret!");
+}
+else{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
