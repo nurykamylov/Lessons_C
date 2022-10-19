67f8ae5e3e613010d4ece241fa82c67ecc1d86a1
Nurik <you@example.com>
2022-10-20 00:23:24 +0300

    example008_IntroArray

---
+++ example008_IntroArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[]array = {13,21,33,42,51,65,71,84,19};
+    int Max(int arg1, int arg2, int arg3)
+    { 
+        int result = arg1;
+        if (arg2 > result) result = arg2;
+        if (arg3 > result) result = arg3;
+        return result;
+    }
+
+int F_max = Max(Max(array[0],array[1],array[2]),
+            Max(array[3],array[4],array[5]),
+            Max(array[6],array[7],array[8]));
+Console.WriteLine(F_max);
